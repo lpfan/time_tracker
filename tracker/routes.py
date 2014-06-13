54a4b08fd13781c69f859b5fcb2e6021b07d6d91
Marko <mark.halayko@gmail.com>
2014-06-13 22:11:27 +0300

    New route to about

--- tracker/routes.py
+++ tracker/routes.py
@@ -1,9 +1,12 @@
+import views
+
 from tracker import app
 from shared_lib.tools import register_routes
 
 
 routes = (
     ('/', views.IndexView, 'index_view'),
+    ('/about', about.AboutPage, 'about_page')
 )
 
 register_routes(routes, app)
