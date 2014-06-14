9be3f97e6470fe657c07f48349566162cb44d789
Misha Shchetinin <misha.shchetinin@gmail.com>
2014-06-14 13:29:53 +0300

    added login_manager and extended user model

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,15 +1,23 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import restful
 from flask.ext.kvsession import KVSessionExtension
+from flask.ext import login
 from simplekv.db.sql import SQLAlchemyStore as KVDBStore
 from flask import Flask
 
+from config.base import BaseConfig
+
 
 app = Flask(__name__)
+app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 api = restful.Api(app)
 
 store = KVDBStore(db.engine, db.metadata, 'sessions')
 KVSessionExtension(store, app)
+
+login_manager = login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login_view'
 
 import routes
--- tracker/shared_lib/tools.py
+++ tracker/shared_lib/tools.py
@@ -3,3 +3,5 @@ def register_routes(routes, app):
     for r_t in routes:
         view_func = r_t[1].as_view(r_t[2])
         app.add_url_rule(r_t[0], view_func=view_func, methods=['GET', 'POST'])
+
+
